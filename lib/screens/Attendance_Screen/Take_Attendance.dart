7d350208fc6700e73e0a140816e321c4bb9b9fbe
ssabbir39 <sabbirsmuct39@gmail.com>
2024-05-12 16:28:19 +0600

    version:12.05.24

--- lib/screens/Attendance_Screen/Take_Attendance.dart
+++ lib/screens/Attendance_Screen/Take_Attendance.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:student_management_system/constants.dart';
-
-import '../../Repository_and_Authentication/services/accounts_db.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 class TakeAttendance extends StatefulWidget {
-  final String courseName; // Add courseName parameter
+  final String courseName;
   const TakeAttendance({Key? key, required this.courseName}) : super(key: key);
 
   @override
@@ -13,7 +15,6 @@ class TakeAttendance extends StatefulWidget {
 }
 
 class _TakeAttendanceState extends State<TakeAttendance> {
-  AccountsDB _accountsDB = AccountsDB();
   List<DocumentSnapshot>? _students = [];
   Map<String, bool> _attendance = {};
   bool loading = false;
@@ -25,28 +26,27 @@ class _TakeAttendanceState extends State<TakeAttendance> {
   }
 
   void _fetchStudents() {
-    setState(() => loading = true); // Move loading state update before fetching data
+    setState(() => loading = true);
     if (widget.courseName != null) {
-      // Use widget.courseName instead of _selectedCourse
       FirebaseFirestore.instance
           .collection('courses')
-          .doc(widget.courseName) // Use widget.courseName instead of _selectedCourse
+          .doc(widget.courseName)
           .collection('students')
           .get()
           .then((querySnapshot) {
         setState(() {
           _students = querySnapshot.docs;
-          loading = false; // Update loading state after fetching data
+          loading = false;
         });
       }).catchError((error) {
         print('Error fetching students: $error');
         setState(() {
           _students = [];
-          loading = false; // Update loading state in case of error
+          loading = false;
         });
       });
     } else {
-      setState(() => loading = false); // Update loading state if no course is selected
+      setState(() => loading = false);
     }
   }
 
@@ -66,7 +66,7 @@ class _TakeAttendanceState extends State<TakeAttendance> {
         title: const Text(
           'Student Attendance',
           style: TextStyle(
-            color: kTextWhiteColor,
+            color: Colors.white,
           ),
         ),
       ),
@@ -78,44 +78,42 @@ class _TakeAttendanceState extends State<TakeAttendance> {
           ? Container(
         decoration: const BoxDecoration(
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(kDefaultPadding),
-            topRight: Radius.circular(kDefaultPadding),
+            topLeft: Radius.circular(16),
+            topRight: Radius.circular(16),
           ),
-          color: kOtherColor,
+          color: Colors.grey,
         ),
         child: ListView.builder(
           itemCount: _students!.length,
           itemBuilder: (context, index) {
             String fullName = _students![index]['fullName'] ?? '';
             String email = _students![index]['email'] ?? '';
-            String studentUid = _students![index].id; // Updated to get document ID
             String studentId = _students![index]['id'] ?? '';
-            bool isPresent = _attendance[studentUid] ?? false; // Use studentUid for attendance
+            bool isPresent = _attendance[studentId] ?? false;
             return Container(
               margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
               decoration: BoxDecoration(
-                color: kPrimaryColor,
+                color: Colors.white,
                 borderRadius: BorderRadius.circular(12),
               ),
               child: ListTile(
                 title: Text(
-                  fullName,
+                  '$fullName (ID: $studentId)',
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 16,
-                    color: kTextWhiteColor,
                   ),
                 ),
                 subtitle: Text(
-                  'ID: $studentId\nEmail: $email',
-                  style: const TextStyle(fontSize: 14, color: kTextWhiteColor),
+                  'Email: $email',
+                  style: const TextStyle(fontSize: 14),
                 ),
                 trailing: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     GestureDetector(
                       onTap: () {
-                        _toggleAttendance(studentUid, true); // Use studentUid
+                        _toggleAttendance(studentId, true);
                       },
                       child: Container(
                         padding: const EdgeInsets.all(8),
@@ -134,7 +132,7 @@ class _TakeAttendanceState extends State<TakeAttendance> {
                     const SizedBox(width: 10),
                     GestureDetector(
                       onTap: () {
-                        _toggleAttendance(studentUid, false); // Use studentUid
+                        _toggleAttendance(studentId, false);
                       },
                       child: Container(
                         padding: const EdgeInsets.all(8),
@@ -163,6 +161,12 @@ class _TakeAttendanceState extends State<TakeAttendance> {
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           submitAllAttendance(widget.courseName);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ReportDownload(docid: widget.courseName, attendanceData: _attendance),
+            ),
+          );
         },
         label: const Text('Submit Attendance'),
         icon: const Icon(Icons.save),
@@ -171,40 +175,35 @@ class _TakeAttendanceState extends State<TakeAttendance> {
     );
   }
 
-  void _toggleAttendance(String studentUid, bool present) {
+  void _toggleAttendance(String studentId, bool present) {
     setState(() {
-      _attendance[studentUid] = present;
+      _attendance[studentId] = present;
     });
   }
 
   Future<void> submitAllAttendance(String className) async {
     try {
-      // Get the collection reference for the attendance of the specific course
       CollectionReference attendanceRef = FirebaseFirestore.instance
           .collection('courses')
           .doc(className)
           .collection('Attendance');
 
-      // Get current date
       DateTime currentDate = DateTime.now();
 
-      // Loop through each student's attendance
       await Future.forEach(_attendance.entries, (entry) async {
-        String studentUid = entry.key;
+        String studentId = entry.key;
         bool present = entry.value;
 
-        // Get student details
         DocumentSnapshot studentSnapshot = await FirebaseFirestore.instance
             .collection('courses')
             .doc(className)
             .collection('students')
-            .doc(studentUid)
+            .doc(studentId)
             .get();
         String studentName = studentSnapshot['fullName'];
-        String studentId = studentSnapshot['id'];
+        // String studentId = studentSnapshot['id'];
 
-        // Get existing attendance data for the current date, if any
-        DocumentSnapshot? existingAttendanceSnapshot = await attendanceRef.doc(studentUid).get();
+        DocumentSnapshot? existingAttendanceSnapshot = await attendanceRef.doc(studentId).get();
         Map<String, dynamic> attendanceData = {
           'name': studentName,
           'id': studentId,
@@ -216,32 +215,28 @@ class _TakeAttendanceState extends State<TakeAttendance> {
           attendanceData = existingAttendanceSnapshot.data() as Map<String, dynamic>;
         }
 
-        // Update attendance count
         if (present) {
           attendanceData['present'] = (attendanceData['present'] ?? 0) + 1;
         } else {
           attendanceData['absent'] = (attendanceData['absent'] ?? 0) + 1;
         }
 
-        // Store attendance data for each student along with date, name, ID, present, and absent counts
-        await attendanceRef.doc(studentUid).set({
+        await attendanceRef.doc(studentId).set({
           'date': currentDate,
           'name': studentName,
           'id': studentId,
           'present': attendanceData['present'],
           'absent': attendanceData['absent'],
-          'uid': studentUid, // Store student UID along with attendance data
+          'uid': studentId,
         });
       });
 
-      // Show success message
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('Attendance submitted successfully'),
         ),
       );
     } catch (e) {
-      // Handle errors
       print('Error submitting attendance: $e');
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
@@ -251,5 +246,95 @@ class _TakeAttendanceState extends State<TakeAttendance> {
       );
     }
   }
+}
 
+class ReportDownload extends StatefulWidget {
+  final String docid;
+  final Map<String, bool> attendanceData;
+  ReportDownload({required this.docid, required this.attendanceData});
+
+  @override
+  State<ReportDownload> createState() => _ReportDownloadState(docid: docid, attendanceData: attendanceData);
+}
+
+class _ReportDownloadState extends State<ReportDownload> {
+  final String docid;
+  final Map<String, bool> attendanceData;
+  _ReportDownloadState({required this.docid, required this.attendanceData});
+
+  @override
+  Future<Uint8List> generateDocument(PdfPageFormat format) async {
+    final doc = pw.Document(pageMode: PdfPageMode.outlines);
+
+    final font1 = await PdfGoogleFonts.openSansRegular();
+    final font2 = await PdfGoogleFonts.openSansBold();
+
+    String? _logo = await rootBundle.loadString('assets/icons/datasheet.svg');
+
+    // Extract attendance data
+    final presentStudents = attendanceData.entries.where((entry) => entry.value).toList();
+    final absentStudents = attendanceData.entries.where((entry) => !entry.value).toList();
+
+    doc.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Column(
+              children: [
+                pw.Column(
+                  children: [
+                    pw.SvgImage(
+                      svg: _logo!,
+                      height: 100,
+                    ),
+                    pw.Text(
+                      'Attendance Report - $docid',
+                      style: pw.TextStyle(fontSize: 30, fontWeight: pw.FontWeight.bold),
+                    ),
+                    pw.Divider(),
+                  ],
+                ),
+                pw.Text(
+                  'Total Students: ${presentStudents.length + absentStudents.length}',
+                  style: pw.TextStyle(fontSize: 18),
+                ),
+                pw.Text(
+                  'Present Students: ${presentStudents.length}',
+                  style: pw.TextStyle(fontSize: 18),
+                ),
+                pw.Text(
+                  'Absent Students: ${absentStudents.length}',
+                  style: pw.TextStyle(fontSize: 18),
+                ),
+                pw.Divider(),
+                pw.Text('Present Students:', style: pw.TextStyle(fontSize: 20, fontWeight: pw.FontWeight.bold)),
+                for (var entry in presentStudents)
+                  pw.Text(
+                    'ID: ${entry.key}',
+                    style: pw.TextStyle(fontSize: 18),
+                  ),
+                pw.Divider(),
+                pw.Text('Absent Students:', style: pw.TextStyle(fontSize: 20, fontWeight: pw.FontWeight.bold)),
+                for (var entry in absentStudents)
+                  pw.Text(
+                    'ID: ${entry.key}',
+                    style: pw.TextStyle(fontSize: 18),
+                  ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+
+    return doc.save();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PdfPreview(
+      build: generateDocument,
+    );
+  }
 }
